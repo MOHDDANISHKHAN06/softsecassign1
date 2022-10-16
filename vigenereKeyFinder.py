89b92361b6fdd54a9a43a0a59ed7d22a352dd9f4
Jayagauri-Sunke <93895226+Jayagauri-Sunke@users.noreply.github.com>
2022-10-16 14:30:42 -0700

    q3 and q4

---
+++ vigenereKeyFinder.py
@@ -0,0 +1,80 @@
+asciiList = [c for c in (chr(i) for i in range(65,91))]
+
+def keyForText(text, key):
+    key = list(key)
+    textLen = len(text)
+    keyLen = len(key)
+    if textLen == keyLen:
+        return(key)
+    else:
+        for i in range(len(text) -keyLen):
+            key.append(key[i % keyLen])
+    return("" . join(key))
+
+def decrypt(cipherText, key,asciiList):
+    text = []
+    for i in range(len(cipherText)):
+        k = asciiList.index(key[i])
+        t = asciiList.index(cipherText[i])
+        temp = asciiList[(t-k) % len(asciiList)]
+        text.append(temp)
+    return("" . join(text))
+
+def keyFinder(pText,cText,asciiList):
+    if len(pText) != len(cText):
+        print("Lengths of the plain text and cipher text do not match")
+        return
+    keyLen = [1,2,3] # Given that the Key length can only be between 1-3
+    # For Key length == 1
+    key = ''
+    keys = []
+    for h in keyLen:
+        print("Keys of length ",h," :")
+        j = 0
+        i = 0
+        while j < len(asciiList):
+            if h == 1:
+                keys.append(asciiList[j])
+                print(asciiList[j])
+            if len(key)!=h:
+                if key and h != 1:
+                    if key+asciiList[j] not in keys:
+                        keys.append(key+asciiList[j])
+                        print(key+asciiList[j])
+                c = asciiList.index(cipherText[i])
+                p = asciiList.index(plainText[i])
+                a = (c-j)%len(asciiList)
+                if a == p:
+                    key +=asciiList[j]
+                    i+=1
+                    j = 0
+                else:
+                    j+=1
+            if key:
+                if len(key) == h:  
+                    keyUpdate = keyForText(pText,key)
+                    result = decrypt(cText,keyUpdate,asciiList)
+                    if result == pText:
+                        print(keys)
+                        with open('output.txt', 'a') as f:
+                            f.write("Keys checked = ")
+                            for k in keys:
+                                f.write(k+", ")
+                            f.write("\nActual key found = "+key+"\n")
+                        return key
+                    else:
+                        key = ''
+                        break
+                    
+                   
+
+plainText = 'ARIZONASTATEUNIVERSITY'
+cipherText = 'EUCDRHEVNEWYYQCZHLWLNC'
+with open('output.txt', 'w') as f:
+    f.writelines("Plain Text = "+plainText+"\nCipher text = "+cipherText+"\n")
+key = keyFinder(plainText,cipherText,asciiList)
+plainText = 'COMPUTERSCIENCE'
+cipherText = 'GRGTXNIUMGLYRFY'
+with open('output.txt', 'a') as f:
+    f.writelines("Plain Text = "+plainText+"\nCipher text = "+cipherText+"\n")
+key = keyFinder(plainText,cipherText,asciiList)
---
+++ vignere_cipher.py
@@ -0,0 +1,80 @@
+asciiList = [c for c in (chr(i) for i in range(32,127))]
+
+
+def keyForText(text, key):
+    key = list(key)
+    textLen = len(text)
+    keyLen = len(key)
+    if textLen == keyLen:
+        return(key)
+    else:
+        for i in range(len(text) -keyLen):
+            key.append(key[i % keyLen])
+    return("" . join(key))
+
+def encrypt(text, key, asciiList):
+    cipherText = []
+    for i in range(len(text)):
+        k = asciiList.index(key[i])
+        t = asciiList.index(text[i])
+        temp = asciiList[(t+k) % len(asciiList)]
+        cipherText.append(temp)
+    return("" . join(cipherText))
+
+def decrypt(cipherText, key,asciiList):
+    text = []
+    for i in range(len(cipherText)):
+        k = asciiList.index(key[i])
+        t = asciiList.index(cipherText[i])
+        temp = asciiList[(t-k) % len(asciiList)]
+        text.append(temp)
+    return("" . join(text))
+
+def vigenereEncrpytDecrypt(text, key, typ):
+    error = 0
+    if not text:
+        print ('No text provided')
+        error += 1
+    if key:
+        if len(key)>3:
+            print("Please enter key with 1-3 characters")
+            error += 1
+        flag = False
+        for i in key:
+            if ord(i) < 65 or ord(i) > 90:
+                flag = True
+        if flag: 
+            print("Please provide only upper case characters")
+            error += 1
+    else:
+        print("No key provided")
+        error += 1
+    if typ not in ('d', 'e'):
+        print ('Provide "d" for decrypt or "e" for encrypt')
+        error += 1
+    
+    if error != 0:
+        print("Number of errors found = ",error)
+        print("Please retry")
+        return
+
+    # if typ == 'e':
+    #     text = text.replace(" ", "")
+    
+    resultText = ''
+    updatedKey = keyForText(text,key)
+
+    if typ == 'e':
+        resultText = encrypt(text,updatedKey,asciiList)
+    else:
+        resultText = decrypt(text,updatedKey,asciiList)
+
+    print(resultText)
+    return resultText
+
+plainText = input("Please input plain text or cipher text = ")
+print(plainText)
+key = input("Please input Key = ")
+encryptDecrypt = input('Provide "d" for decrypt or "e" for encrypt = ')
+vigenereEncrpytDecrypt(plainText,key,encryptDecrypt)
+
